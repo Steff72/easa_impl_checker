37ebcdd13e73e6a643d6ba6f6b45813e5b7ba522
Stefan Binkert <stefanbinkert@Mac-mini-von-Stefan.local>
2023-11-13 16:21:45 +0100

    input added

--- core.py
+++ core.py
@@ -26,11 +26,9 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
     )
     return qa({"question": query, "chat_history": chat_history})['answer']
 
-easa_text = "The regulation FCL.050 from the 'Easy Access Rules for Aircrew (Regulation (EU) No 1178/2011) - Revision from August 2023' pertains to the recording of flight time. It includes details on how pilots should document their flight time. This regulation is crucial for ensuring that pilots maintain an accurate and verifiable record of their flying experience. The Acceptable Means of Compliance (AMC) and Guidance Material (GM) for FCL.050 provide additional information and guidance on how to properly record flight time in the pilot logbook, ensuring compliance with the regulation's requirements"
-
-promt = f"Please conduct a detailed analysis of the attached documents for compliance with the following regulations: [{easa_text}]. It's crucial to provide specific section and paragraph numbers for each instance where a regulation is mentioned, addressed, or implied, even if they are in the same section as a previous instance. For every instance found, give a detailed reference in the format: 'Document Name, Section:Paragraph number'. Ensure that both section and paragraph numbers are included for each instance. If a regulation is mentioned multiple times in the same section, list each occurrence separately with its specific paragraph number."
-
 
 if __name__ == "__main__":
+    easa_text = input("Enter EASA FCL summary: ")
+    promt = f"Please conduct a detailed analysis of the context for compliance with the following regulations: [{easa_text}]. It's crucial to provide specific section and paragraph numbers for each instance where a regulation is mentioned, addressed, or implied, even if they are in the same section as a previous instance. For every instance found, give a detailed reference in the format: 'Document Name, Section:Paragraph number'. Ensure that both section and paragraph numbers are included for each instance. If a regulation is mentioned multiple times in the same section, list each occurrence separately with its specific paragraph number."
     
     print(run_llm(promt))
