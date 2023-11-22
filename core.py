a176781641314eca88109ca1ade4b0c045dfc855
Stefan Binkert <sbinkert@me.com>
2023-11-22 16:51:19 +0100

    frontend added

--- core.py
+++ core.py
@@ -24,11 +24,14 @@ def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
         retriever=docsearch.as_retriever(search_kwargs={"k": 20}),
         return_source_documents=True,
     )
-    return qa({"question": query, "chat_history": chat_history})["answer"]
+
+    promt = f"Please conduct a detailed analysis of the context for compliance with the following regulations: [{query}]. It's crucial to provide specific section and paragraph numbers for each instance where a regulation is mentioned, addressed, or implied, even if they are in the same section as a previous instance. For every instance found, give a detailed reference in the format: 'Document Name, Section:Paragraph number'. Ensure that both section and paragraph numbers are included for each instance. If a regulation is mentioned multiple times in the same section, list each occurrence separately with its specific paragraph number."
+
+    return qa({"question": promt, "chat_history": chat_history})["answer"]
 
 
 if __name__ == "__main__":
-    easa_text = input("Enter EASA FCL summary: ")
-    promt = f"Please conduct a detailed analysis of the context for compliance with the following regulations: [{easa_text}]. It's crucial to provide specific section and paragraph numbers for each instance where a regulation is mentioned, addressed, or implied, even if they are in the same section as a previous instance. For every instance found, give a detailed reference in the format: 'Document Name, Section:Paragraph number'. Ensure that both section and paragraph numbers are included for each instance. If a regulation is mentioned multiple times in the same section, list each occurrence separately with its specific paragraph number."
-
-    print(run_llm(promt))
+ 
+    easa_text = "the holder of an instructor certificate may log as PIC all flight time during which he or she acts as an instructor in an aircraft;"    
+    print("*" * 60)
+    print(run_llm(easa_text))
